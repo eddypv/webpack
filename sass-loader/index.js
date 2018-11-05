1c749e03eae3bcf663ab739f66484be18936dedb
eddypv <eddypv0@gmail.com>
2018-11-05 09:52:13 -0400

    [UPDATE] Sass loader

---
+++ sass-loader/index.js
@@ -0,0 +1,16 @@
+import './estilos.css'
+import {firstMessage} from './message.js'
+import platziImg from './log.png'
+import React, {Component} from 'react';
+import {render} from 'react-dom'
+import Teachers from './components/teachers.js'
+import data from './teachers.json'
+
+
+render(<Teachers  data= {data}/>, document.getElementById('container'))
+document.write(firstMessage)
+const img = document.createElement('img')
+img.setAttribute('src', platziImg)
+document.body.append(img)
+
+console.log('Con estilos css')
